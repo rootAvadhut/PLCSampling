a504523febabf3fab28425db45a27bdcf5e1f1fb
avadhut <avadhutshinde92@gmail.com>
2024-08-28 10:42:08 +0530

    ui-master

--- ui.py
+++ ui.py
@@ -1,15 +1,19 @@
 import tkinter as tk
-# from file_operations import browse_file
-# from data_sampling import sample_data
+import tkinter.font as tkFont
 
 def run_application():
     root = tk.Tk()
     root.title("PLC Interface")
+    
+    # Set the geometry of the window (width x height + x_offset + y_offset)
+    root.geometry("500x250+300+200")
+
+    custom_font = tkFont.Font(family="Helvetica", size=10)
 
     # Create and place labels and entry fields
     tk.Label(root, text="IP Address:").grid(row=0, column=0, padx=10, pady=5, sticky='e')
     ip_entry = tk.Entry(root, width=30)
-    ip_entry.grid(row=0, column=1, padx=10, pady=5)
+    ip_entry.grid(row=0, column=1, padx=10, pady=5, sticky='w')
 
     tk.Label(root, text="Port No.:").grid(row=1, column=0, padx=10, pady=5, sticky='e')
     port_entry = tk.Entry(root, width=10)  # Adjusted width for 6-digit input
@@ -29,8 +33,8 @@ def run_application():
     # Adding the label "(change in %)" next to Change in Data
     tk.Label(root, text="(change in %)").grid(row=3, column=2, padx=5, pady=5, sticky='w')
 
-
-
-   
     # Run the application
     root.mainloop()
+
+# Start the application
+run_application()
